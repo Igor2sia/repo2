dfc6c501342eeb2525cfc70eea4d9ed07054cb11
Your Name <you@example.com>
2025-03-24 11:45:52 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("1");
+        System.out.print("2");
+
     }
 }
